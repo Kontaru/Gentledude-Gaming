a01f67705e82f5fd2f416ff1af35ecbc61849b15
Kontaru <drwafflesxd@gmail.com>
2018-01-30 19:14:30 +0000

    HYL: Fixed Distribution Quest bug

--- Monsters Managing Monsters/Assets/_Minigames Scripts/HYL_IS_Distribution.cs
+++ Monsters Managing Monsters/Assets/_Minigames Scripts/HYL_IS_Distribution.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HYL_IS_Distribution : MonoBehaviour {
+public class HYL_IS_Distribution : QuestPart {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public InteractableObject[] items;
+    public bool BL_QuestComplete;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (BL_IsInteractable)
+        {
+            foreach (InteractableObject obj in items)
+            {
+                if (obj.target != null)
+                    obj.target.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (InteractableObject obj in items)
+            {
+                if (obj.target != null)
+                    obj.target.SetActive(false);
+            }
+        }
+
+        CheckEndCondition();
+
+        BL_QuestComplete = true;
+
+        foreach (InteractableObject obj in items)
+        {
+            obj.CheckInteractState(GameManager.instance.Player);
+
+            if (obj.acquired)
+                Destroy(obj.target);
+
+            if (obj.target != null)
+                BL_QuestComplete = false;
+
+        }
+
+        if (BL_QuestComplete)
+            BL_MinigameComplete = true;
+    }
 }
