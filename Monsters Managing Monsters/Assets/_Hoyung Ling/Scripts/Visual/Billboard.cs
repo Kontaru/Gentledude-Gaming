c2c3c5c20d71cc012527cbba2027792d9638cca3
Unknown <drwafflesxd@gmail.com>
2018-02-08 13:46:37 +0000

    HYL: Billboard to cam

--- Monsters Managing Monsters/Assets/_Hoyung Ling/Scripts/Visual/Billboard.cs
+++ Monsters Managing Monsters/Assets/_Hoyung Ling/Scripts/Visual/Billboard.cs
@@ -7,6 +7,7 @@ public class Billboard : MonoBehaviour
 
     //Should we look at the camera?
     public bool BL_LookAtPC = false;
+    public bool BL_LookAtCam = false;
 
     private GameObject player;
     private Quaternion originalRot;
@@ -33,17 +34,26 @@ public class Billboard : MonoBehaviour
             BL_LookAtPC = false;
         }
 
-        if(Vector3.Distance(gameObject.transform.position, player.transform.position) < 15f)
+        if (BL_LookAtPC)
+        {
+            if (Vector3.Distance(gameObject.transform.position, player.transform.position) < 15f)
+            {
+                //originalRot = transform.rotation;
+                lookPos = player.transform.position - transform.position;
+                lookPos.y = 0;
+                rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, playerfollowSmoothSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, originalRot, playerfollowSmoothSpeed * Time.deltaTime);
+            }
+        }else if (BL_LookAtCam)
         {
             //originalRot = transform.rotation;
-            lookPos = transform.position - player.transform.position;
-            lookPos.y = 0;
+            lookPos = Camera.main.transform.position - transform.position;
             rotation = Quaternion.LookRotation(lookPos);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, playerfollowSmoothSpeed * Time.deltaTime);
-        }
-        else
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, originalRot, playerfollowSmoothSpeed * Time.deltaTime);
         }
     }
 }
