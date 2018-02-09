442ce3bcd20cd623ad595b6cfa123575d799b798
Dean Cabral <u1503139@uel.ac.uk>
2018-02-09 20:55:51 +0000

    Dean: Code clean-up

--- Monsters Managing Monsters/Assets/_Hoyung Ling/Scripts/Visual/Billboard.cs
+++ Monsters Managing Monsters/Assets/_Hoyung Ling/Scripts/Visual/Billboard.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-
     //Should we look at the camera?
     public bool BL_LookAtPC = false;
     public bool BL_LookAtCam = false;
@@ -29,7 +28,22 @@ public class Billboard : MonoBehaviour
     // Update is called once per frame
     virtual public void Update()
     {
-        if (BL_LookAtPC)
+        if (Vector3.Distance(gameObject.transform.position, player.transform.position) < 10f && !BL_LookAtCam)
+        {
+            //originalRot = transform.rotation;
+            lookPos = transform.position - player.transform.position;
+            lookPos.y = 0;
+            rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, playerfollowSmoothSpeed * Time.deltaTime);
+        }
+        else
+        {
+            lookPos = transform.position - Camera.main.transform.position;
+            rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, playerfollowSmoothSpeed * Time.deltaTime);
+        }
+
+        /*if (BL_LookAtPC)
         {
             if (Vector3.Distance(gameObject.transform.position, player.transform.position) < 15f)
             {
@@ -43,12 +57,13 @@ public class Billboard : MonoBehaviour
             {
                 transform.rotation = Quaternion.Slerp(transform.rotation, originalRot, playerfollowSmoothSpeed * Time.deltaTime);
             }
+
         }else if (BL_LookAtCam)
         {
             //originalRot = transform.rotation;
             lookPos = transform.position - Camera.main.transform.position;
             rotation = Quaternion.LookRotation(lookPos);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, playerfollowSmoothSpeed * Time.deltaTime);
-        }
+        }*/
     }
 }
