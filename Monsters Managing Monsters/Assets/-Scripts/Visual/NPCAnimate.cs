5d59e663a6d99759af048f8faa872652e9dac2a3
Dean Cabral <u1503139@uel.ac.uk>
2018-04-13 21:23:46 +0100

    Dean: Attention to details

---
+++ Monsters Managing Monsters/Assets/-Scripts/Visual/NPCAnimate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCAnimate : MonoBehaviour
+{
+
+    public bool BL_canAmimate;
+    public Sprite[] animSteps;
+    public float delay;
+
+    private SpriteRenderer SR;
+    private bool isAnimating;
+
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+        StartCoroutine(Animate(delay));
+    }
+
+    private void Update()
+    {
+        if (BL_canAmimate && !isAnimating) StartCoroutine(Animate(delay));
+    }
+
+    IEnumerator Animate(float delay)
+    {
+        isAnimating = true;
+
+        for (int i = 0; i < animSteps.Length; i++)
+        {
+            SR.sprite = animSteps[i];
+            yield return new WaitForSeconds(delay);
+        }
+
+        isAnimating = false;
+    }
+}
