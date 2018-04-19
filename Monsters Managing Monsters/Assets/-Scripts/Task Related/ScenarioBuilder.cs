3c80310e89737fc4b50a9054ba150497fc7e0fea
Kontaru <drwafflesxd@gmail.com>
2018-04-19 09:17:03 +0100

    HYL: CurrentTasks v1.5.0

---
+++ Monsters Managing Monsters/Assets/-Scripts/Task Related/ScenarioBuilder.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ScenarioBuilder : MonoBehaviour {
+
+    public List<Task> PlayerQuests = new List<Task>();           //Generated - Display this as the top 5 possible quests
+    public List<Task> DailyQuests;
+    List<Task> PreSortedDailyQuests;
+
+    public int APlim;
+    TaskManager taskManager;
+
+	// Use this for initialization
+	void Start () {
+        taskManager = TaskManager.instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (DayCycle.instance.BL_beginDay)
+        {
+            GenerateDailyQuests(taskManager.Tasks);
+        }
+	}
+
+    void GenerateDailyQuests(Task[] all)
+    {
+        //Sort based on max spendable AP
+        int spendableAP = APlim;
+        int questLims = spendableAP + 5;
+        List<Task> allquests = TaskManager.instance.Tasks.ToList();
+        List<Task> dailyQuests = new List<Task>();
+
+        for (int current = 0; current < allquests.Count; current++)
+        {
+            if (allquests[current].Quest_Finish)
+                break;
+            else
+            {
+                int tQuestLims = 0;
+                if (tQuestLims == 0)
+                    dailyQuests.Add(allquests[current]);
+                else
+                {
+                    for (int index = 0; index < dailyQuests.Count; index++)
+                    {
+                        tQuestLims += dailyQuests[index].IN_actionPointWeight;
+                    }
+                    if (tQuestLims + allquests[current].IN_actionPointWeight >= questLims)
+                        break;
+                    else
+                        dailyQuests.Add(allquests[current]);
+                }
+            }
+        }
+
+        PreSortedDailyQuests = dailyQuests;
+    }
+
+    void RankDailyQuests(List<Task> preSorted)
+    {
+        List<Task> sorted = preSorted;
+
+        sorted.Sort(delegate (Task x, Task y) { return x.IN_actionPointWeight.CompareTo(y.IN_actionPointWeight); });
+
+        DailyQuests = sorted;
+    }
+
+    void GeneratePlayerPossibilities(List<Task> dailyQuests)
+    {
+        List<Task> shuffle = dailyQuests;
+        List<Task> tempList = new List<Task>(shuffle.Count);
+
+        List<Task> playerQuests = new List<Task>();
+
+        for (int current = 0; current < shuffle.Count; current++)
+        {
+            Task storedTask = shuffle[current];
+            int searchedTask = Random.Range(current, shuffle.Count);
+            shuffle[current] = shuffle[searchedTask];
+            shuffle[searchedTask] = storedTask;
+        }
+
+        for (int current = 0; current < DailyQuests.Count || current < 5; current++)
+        {
+            playerQuests[current] = shuffle[current];
+        }
+
+        PlayerQuests = playerQuests;
+    }
+}
