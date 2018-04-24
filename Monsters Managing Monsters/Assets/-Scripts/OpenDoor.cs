e31f703aa42f790bf17fd9fc26bc69a720122710
Kontaru <drwafflesxd@gmail.com>
2018-04-25 00:29:35 +0100

    HYL: Door opening for minigames

---
+++ Monsters Managing Monsters/Assets/-Scripts/OpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    public static OpenDoor instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    Animation anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animation>();
+	}
+	
+	public void Open()
+    {
+        anim.Play();
+    }
+
+    public void Close()
+    {
+        anim.Rewind();
+    }
+}
