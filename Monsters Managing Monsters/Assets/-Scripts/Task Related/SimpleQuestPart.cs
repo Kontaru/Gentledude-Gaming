82a66336cb71183308f03259a72b858ad138781d
Kontaru <drwafflesxd@gmail.com>
2018-05-15 19:51:17 +0100

    HYL: Forgot to save door fix

--- Monsters Managing Monsters/Assets/-Scripts/Task Related/SimpleQuestPart.cs
+++ Monsters Managing Monsters/Assets/-Scripts/Task Related/SimpleQuestPart.cs
@@ -64,7 +64,6 @@ public class SimpleQuestPart : QuestPart {
 
         if(BL_IsInteractable)
         {
-            OpenDoor.instance.Open();
             foreach (GameObject target in VisibilityTargets)
             {
                 target.SetActive(true);
